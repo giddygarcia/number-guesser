7fd1e5c90896c2618c92ea66f693458801246c1d
giddyg <christine.garcia@obf.ateneo.edu>
2019-11-01 20:12:56 +0800

    Add functional game JS

--- script.js
+++ script.js
@@ -11,17 +11,32 @@ const generateCompGuess = () => {
 }
 
 const compareGuesses = (playerGuess, computerGuess, targetGuess) => {
-	const playerDiff = Math.abs(playerGuess - targetGuess)
-	const compDiff = Math.abs(computerGuess - targetGuess)
+	const playerDiff = Math.abs(targetGuess - playerGuess)
+	const compDiff = Math.abs(targetGuess - computerGuess)
 	return playerDiff <= compDiff;
 }
 
 const updateScore = (winner) => {
-	if  (winner === 'player') {
+	if (winner === 'player')
 		playerScore++;
-	}
 	else if (winner === 'computer')
 		computerScore++;
+}
+
+const newRound = () => {
+	nextRoundBtn.setAttribute('disabled', true);
+	guessBtn.removeAttribute('disabled');
+	addButton.removeAttribute('disabled');
+
+	targetNumDisplay.innerText = '?';
+	computerGuessDisplay.innerText = '?';
+	playerGuessInput.innerText = '0';
+	computerWinsDisplay.innerText = '';
+	guessBtn.innerText = 'Make This Guess';
+	guessBtn.style.backgroundColor = '';
+	guessBtn.style.color = '';
+	guessBtn.style.fontWeight = '';
+	count = 0;
 }
 
 const advanceRound = () => currentRoundNumber++;
@@ -37,32 +52,74 @@ let targetNumDisplay = document.getElementById('target-number');
 let playerScoreDisplay = document.getElementById('player-score');
 let playerGuessInput = document.getElementById('player-guess');
 
-let guessButton = document.getElementById('btn-guess');
-let nextRoundButton = document.getElementById('btn-next');
+let guessBtn = document.getElementById('guess-btn');
+let nextRoundBtn = document.getElementById('next-btn');
 
-
-guessButton.addEventListener('click', () => {
+guessBtn.addEventListener('click', () => {
 	target = generateTarget();
-	const currentPlayerGuess = playerGuessInput.value;
 	const computerGuess = generateCompGuess();
 
 	computerGuessDisplay.innerText = computerGuess;
 	targetNumDisplay.innerText = target;
 
-	const playerIsWinner = compareGuesses(currentplayerGuess, computerGuess, target)
+	const playerIsWinner = compareGuesses(count, computerGuess, target)
 	const winner = playerIsWinner ? 'player' : 'computer'
 
 	updateScore(winner);
 
 	if(playerIsWinner) {
-		guessButton.innerText = 'Congrats, You Win!';
-	}
+		guessBtn.innerText = 'Congrats, You Win!';
+		guessBtn.style.backgroundColor = "#39CCCC";
+		guessBtn.style.color = "#01FF70";
+		guessBtn.style.fontWeight = "900";
+	} 
 	else
 		computerWinsDisplay.innerText = 'Computer Wins :|';
 
 	playerScoreDisplay.innerText = playerScore;
 	computerScoreDisplay.innerText = computerScore;
 
-	guessButton.setAttribute('disabled', true)
-	nextRoundButton.removeAttribute('disabled');
+	subtractButton.setAttribute('disabled', true);
+	addButton.setAttribute('disabled',true);
+	guessBtn.setAttribute('disabled', true)
+	nextRoundBtn.removeAttribute('disabled');
+});
+
+nextRoundBtn.addEventListener('click', () => {
+	advanceRound();
+	roundNumDisplay.innerText = currentRoundNumber;
+
+	newRound();
+});
+
+const addButton = document.getElementById('add-btn');
+const subtractButton = document.getElementById('sub-btn');
+var count = 0;
+
+addButton.addEventListener('click', () => {
+	count++;
+	playerGuessInput.innerText = count;
+	handleNumLimits(count);
+});
+
+subtractButton.addEventListener('click', () => {
+	count--;
+	playerGuessInput.innerText = count;
+	handleNumLimits(count);
+});
+
+const handleNumLimits = value => {
+  if (value > 0 && value <= 9) {
+    subtractButton.removeAttribute('disabled');
+    addButton.removeAttribute('disabled');
+  } 
+  else if (value > 9)
+    addButton.setAttribute('disabled', true);
+  else if (value <= 0) {
+    subtractButton.setAttribute('disabled', true);
+  }
+}
+
+playerGuessInput.addEventListener('input', function(e) {
+  handleNumLimits(e.target.value);
 });
